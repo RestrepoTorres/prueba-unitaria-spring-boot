79797a73ddea4f6f6a64f2487c2477acf9ddc116
Victor Restrepo <vmanuel.restrepo@udea.edu.co>
2023-05-09 17:13:35 -0500

    Inicio de proyecto

---
+++ src/main/java/com/api/rest/pruebaunitariasspringboot/PruebaUnitariasSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.api.rest.pruebaunitariasspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaUnitariasSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaUnitariasSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/rest/pruebaunitariasspringboot/PruebaUnitariasSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.rest.pruebaunitariasspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaUnitariasSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
